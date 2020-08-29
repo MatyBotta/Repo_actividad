963f3375f60bf8f79f597687d1014b86243109e4
dejameponerunnombre <68668425+dejameponerunnombre@users.noreply.github.com>
2020-08-28 22:31:28 -0300

    Update Bottarini Aplic Ejercicio1.cs

--- Bottarini Aplic Ejercicio1.cs
+++ Bottarini Aplic Ejercicio1.cs
@@ -6,7 +6,7 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-         double n1, n2, n3, prom;
+         double n1, n2, n3, prom,max=0;
          ("Console.WriteLineIngrese la primer nota");
          n1 = Double.Parse(Console.ReadLine());
          Console.WriteLine("Ingrese la segunda nota");
@@ -15,6 +15,9 @@ namespace Ejercicio1
          prom = (n1 + n2 + n3) / 3;
          Console.WriteLine("El promedio es {0}", prom);
          Console.ReadKey();
+         if (prom>max)
+             max=prom;
+         
 
         }
     }
