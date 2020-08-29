982aae636371343692905bf614df7f6abd3759cc
MatyBotta <Bottarinimatias03@gmail.com>
2020-08-28 22:22:00 -0300

    Create Bottarini Aplic Ejercicio1.cs

---
+++ Bottarini Aplic Ejercicio1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         double n1, n2, n3, prom;
+         ("Console.WriteLineIngrese la primer nota");
+         n1 = Double.Parse(Console.ReadLine());
+         Console.WriteLine("Ingrese la segunda nota");
+         Console.WriteLine("Ingrese la tercer nota");
+         n3 = Double.Parse(Console.ReadLine());
+         prom = (n1 + n2 + n3) / 3;
+         Console.WriteLine("El promedio es {0}", prom);
+         Console.ReadKey();
+
+        }
+    }
+}
